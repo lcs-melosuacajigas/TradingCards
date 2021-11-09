e77aaca9a7a5d4cbb880d482f477d9fa3a07eb16
Mateo Elosua <melosua@lcs.on.ca>
2021-11-09 11:12:29 -0500

    Initial Commit

---
+++ FinalAssignment/FinalAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalAssignmentApp.swift
+//  FinalAssignment
+//
+//  Created by Mateo Elosua on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct FinalAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
